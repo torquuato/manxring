9b495134211c722dbb9edc443a747d233c5c8cb0
torquuato <markdimayuga06@gmail.com>
2025-12-04 01:34:10 +0000

    added members content

--- onionring-widget.js
+++ onionring-widget.js
@@ -30,7 +30,7 @@ if (thisIndex == null) {
   tag.insertAdjacentHTML('afterbegin', `
 <table>
   <tr>
-    <td>This site isn't part of the ${ringName} webring yet. You should talk to the manager to have your site added to the list!</td>
+    <td>This site isn't part of ${ringName} yet. Hold on tight!</td>
   </tr>
 </table>
   `);
